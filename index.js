6978588c4d61048b499853bf82ffee772d5364be
Mabsky <grimdango@gmail.com>
2022-02-12 13:18:33 +1100

    update

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Em_name',
+            message: "Input Employee Name:",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Input Employee ID:",
+        },
+        {
+            type: 'input',
+            name: 'Email',
+            message: "Input Employee Email:",
+        },
+    ])
+}
+
+const create = () => {
+    promptUser()
+    .then (function (answers) {
+        let empys = new Employee(answers.Em_name, answers.id, answers.Email)
+        console.log(empys); 
+        console.log(answers.Em_name);
+    }) 
+    .then ()
+    .catch ()   
+};
+
+create()
+
 class Employee {
-    constructor(name, id, email) {
-      this.name = name;
+    constructor(em_name, id, email) {
+      this.em_name = em_name;
       this.id = id;
       this.email = email;
     }
-    getname() {
-        return inquirer.prompt([ {type: 'input',
-        name: 'Title',
-        message: "Input Project Title:",}])
-    
-    }
-    getID() {
-        //function placeholder
-    }
-    getEmail() {
-        //function placeholder
-    }
-    getRole() {
-        return Employee
-    }
-  }
+}
+
+
+//prompt, then store in class, create employees, (prompt if add'l) then build html, test  
+
+// const miguel = new Employee('miguel', 1, 'miguel@icloud.com');
+// miguel.getName().then(data => {
+//    console.log(data)
+// })
+
+
+/*
+
+const newbie = new Employee()
+
 
   class Manager {
     constructor(name, id, email, officeNumber) {
@@ -92,4 +124,15 @@ class Employee {
   }
 
 
-Employee.getname()
+
+ CODE SAMPLES FOR RUNNING INQUIRER
+const miguel = new Employee('miguel', 1, 'miguel@icloud.com');
+miguel.getName().then(data => {
+   console.log(data)
+})
+
+Employee.prototype.getName().then((value) => {
+    console.log(value)
+})
+
+*/
